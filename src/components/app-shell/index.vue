05e31351e2f70c4ec69dd337b859cabd38f9d736
Bob Bol <bob@voorhoede.nl>
2019-11-11 22:26:21 +0100

    vuetify :: basic layout :: basic config

---
+++ src/components/app-shell/index.vue
@@ -0,0 +1,37 @@
+<template>
+  <v-app>
+    <sidebar />
+
+    <v-app-bar
+      class="app-header"
+      app color="indigo"
+      dark
+    >
+      <v-toolbar-title>
+        RA2CE
+      </v-toolbar-title>
+    </v-app-bar>
+
+    <v-content>
+      <!-- <app-map /> -->
+    </v-content>
+
+    <welcome-dialog />
+
+  </v-app>
+</template>
+
+<script>
+// import AppMap from '@/components/app-map';
+import Sidebar from '@/components/app-shell/sidebar';
+import WelcomeDialog from './welcome-dialog';
+
+export default {
+  components: {
+    Sidebar,
+    WelcomeDialog
+  }
+};
+</script>
+
+<style src="@/assets/css/index.css"></style>
