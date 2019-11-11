6651eadeb84bc250e88d423432d6e7a13be9a9df
Bob Bol <bob@voorhoede.nl>
2019-11-11 16:54:14 +0100

    setup vanilla vue project

--- _version_albania/.eslintrc.js
+++ _version_albania/.eslintrc.js
@@ -1,17 +1,22 @@
 module.exports = {
   root: true,
   env: {
+    browser: true,
     node: true
-  },
-  'extends': [
-    'plugin:vue/essential',
-    'eslint:recommended'
-  ],
-  rules: {
-    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
   },
   parserOptions: {
     parser: 'babel-eslint'
+  },
+  extends: [
+    'plugin:vue/recommended'
+  ],
+  // required to lint *.vue files
+  plugins: [
+    'vue'
+  ],
+  // add your custom rules here
+  rules: {
+    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
   }
 }
