c468e28d1298fbf1df394c0daf29e6ce62e0fc2a
Sjoerd Beentjes <mail@sjoerdbeentjes.nl>
2019-11-14 15:56:35 +0100

    refactor(welcome-dialog): update styles

--- src/components/app-shell/welcome-dialog.vue
+++ src/components/app-shell/welcome-dialog.vue
@@ -2,6 +2,7 @@
   <v-dialog
     v-model="showWelcomeDialog"
     max-width="640"
+    persistent
   >
     <v-card>
       <v-card-title>
@@ -20,7 +21,7 @@
       <v-card-text>
         Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
       </v-card-text>
-      <v-card-actions>
+      <v-card-actions class="px-6 pb-6">
         <v-spacer></v-spacer>
         <v-btn
           class="primary"
@@ -46,6 +47,7 @@ export default {
   .welcome-dialog__images {
     display: flex;
     padding: 0 1.5rem 1.5rem;
+    align-items: flex-start;
   }
 
   .welcome-dialog__images img {
